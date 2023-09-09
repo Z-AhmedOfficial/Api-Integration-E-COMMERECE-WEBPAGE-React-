3e2326d3db5c31beebcb2aaf77de697d342e9bda
Z-AhmedOfficial <128061591+Z-AhmedOfficial@users.noreply.github.com>
2023-09-09 17:19:13 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <div className="App">
       <button disabled={true} onClick={toggleProductList}>Show Product List</button>
       <h1>E-COMMERCE WEBSITE</h1>
-      <img src='https://b.kisscc0.com/20180517/pq/kisscc0-marketing-retail-shopping-e-commerce-clothing-5afdaadfc01594.4132037015265737917868.jpg' width={"100%"} height={"70%"}/>
+      <img alt='homepicture' src='https://b.kisscc0.com/20180517/pq/kisscc0-marketing-retail-shopping-e-commerce-clothing-5afdaadfc01594.4132037015265737917868.jpg' width={"100%"} height={"70%"}/>
       {showProductList && <Link to="/">Go to Product List</Link>}
      < AppRoutes/>
     </div>
